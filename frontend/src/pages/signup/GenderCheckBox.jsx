745a546fde9670d1a028a6190125a0d367a57879
7deb <deb07ban@gmail.com>
2024-07-13 15:48:49 +0530

    fixed typos

--- frontend/src/pages/signup/GenderCheckBox.jsx
+++ frontend/src/pages/signup/GenderCheckBox.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 
-const GenderCheckBox = ({ onCheckboxChange, selectGender }) => {
+const GenderCheckBox = ({ onCheckboxChange, selectedGender }) => {
   return (
     <div className="flex">
       <div className="form-control">
-        <label className={`label gap-3 cursor-pointer ${selectGender === "male" ? "selected" : ""}`}>
+        <label className={`label gap-3 cursor-pointer ${selectedGender === "male" ? "selected" : ""}`}>
           <span className="label-text">Male</span>
           <input type="checkbox" className="checkbox border-slate-900"
-            checked={selectGender === "male"}
+            checked={selectedGender === "male"}
             onChange={() => onCheckboxChange("male")}
           />
         </label>
       </div>
       <div className="form-control">
-        <label className={`label gap-3 cursor-pointer ${selectGender === "female" ? "selected" : ""}`}>
+        <label className={`label gap-3 cursor-pointer ${selectedGender === "female" ? "selected" : ""}`}>
           <span className="label-text">Female</span>
           <input type="checkbox" className="checkbox border-slate-900"
-            checked={selectGender === "female"}
+            checked={selectedGender === "female"}
             onChange={() => onCheckboxChange("female")}
           />
         </label>
